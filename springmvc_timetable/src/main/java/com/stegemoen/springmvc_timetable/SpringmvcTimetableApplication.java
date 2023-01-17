9662af50d48a18f377cfea9e1d5ee26b8ede723a
Helge Stegemoen <helge@stegemoen.com>
2023-01-17 21:08:06 +0100

    spring initializr project

---
+++ springmvc_timetable/src/main/java/com/stegemoen/springmvc_timetable/SpringmvcTimetableApplication.java
@@ -0,0 +1,13 @@
+package com.stegemoen.springmvc_timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcTimetableApplication.class, args);
+	}
+
+}
---
+++ springmvc_timetable/src/test/java/com/stegemoen/springmvc_timetable/SpringmvcTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stegemoen.springmvc_timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcTimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
